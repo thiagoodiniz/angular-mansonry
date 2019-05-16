a7b3a6fb5663abccbe4b6738fbe8eb05ae631660
Thiago Diniz <thiago.diniz@colmeia.me>
2019-05-16 15:54:20 -0300

    Initial commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to test-masonry!');
+    expect(page.getTitleText()).toEqual('Welcome to angular-masonry!');
   });
 
   afterEach(async () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'test-masonry';
+export class AppComponent implements OnInit {
+  title = 'angular-masonry';
+
+  constructor(){}
+
+  ngOnInit(){
+  }
+
+  ngAfterViewChecked() {
+    this.resizeAllGridItems();
+  }
+
+  ngAfterViewInit(){
+      this.resizeAllGridItems();
+  }
+
+  resizeGridItem(item){
+    const grid = document.getElementsByClassName("grid")[0];
+    const rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+    const rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+    const rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+      item.style.gridRowEnd = "span "+rowSpan;
+  }
+
+  resizeAllGridItems(){
+    const allItems = document.getElementsByClassName("item");
+    for(let x=0;x<allItems.length;x++){
+      this.resizeGridItem(allItems[x]);
+    }
+  }
+
+  resizeInstance(instance){
+    const item = instance.elements[0];
+    this.resizeGridItem(item);
+  }
+
 }
